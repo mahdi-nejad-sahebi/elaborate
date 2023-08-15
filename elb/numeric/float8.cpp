991c56ae944dfcfbcf0fd1493bef5850c33c1743
Mahdi Nejad Sahebi <m.nejadsahebi@live.co.uk>
2023-08-15 11:25:27 +0330

    Base files of float8 module were added.

---
+++ elb/numeric/float8.cpp
@@ -0,0 +1,10 @@
+#include "float8_t.hpp"
+
+namespace elb
+{
+  float8_t::float8_t() : m_num(0)
+  {
+    
+  }
+   
+}
---
+++ elb/numeric/float8.hpp
@@ -0,0 +1,15 @@
+#ifndef ELB_FLOAT8_H_
+#defien ELB_FLOAT8_H_
+
+#include <cstdint>
+
+namespace elb
+{
+  struct float8_t
+  {
+    float8_t();
+    int8_t m_num;
+  };
+}
+
+#endif // ELB_FLOAT8_H_
