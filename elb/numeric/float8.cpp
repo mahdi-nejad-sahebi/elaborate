6d79315fcb8feef42316519e6b922b6de553257d
Mahdi Nejad Sahebi <m.nejadsahebi@live.co.uk>
2023-08-16 15:36:57 +0330

    '=' operators were defined for built-in types for reading and writing.

--- elb/numeric/float8.cpp
+++ elb/numeric/float8.cpp
@@ -1,5 +1,7 @@
 #include "float8.hpp"
 
+#include <iostream>
+
 namespace elb
 {
   float8_t::float8_t() : m_num(0)
@@ -7,4 +9,110 @@ namespace elb
     
   }
    
+  float8_t float8_t::operator=(const float& _num)
+  {
+    float8_t float8;
+    return float8;
+  }
+
+  float8_t float8_t::operator=(const uint64_t& _num)
+  {  
+    float8_t float8;
+    return float8;
+  }
+ 
+  float8_t float8_t::operator=(const int64_t& _num)
+  {
+    float8_t float8;
+    return float8;
+  }
+
+  float8_t float8_t::operator=(const uint32_t& _num)
+  {  
+    float8_t float8;
+    return float8;
+  }
+ 
+  float8_t float8_t::operator=(const int32_t& _num)
+  {
+    float8_t float8;
+    return float8;
+  }
+
+  float8_t float8_t::operator=(const uint16_t& _num)
+  {  
+    float8_t float8;
+    return float8;
+  }
+ 
+  float8_t float8_t::operator=(const int16_t& _num)
+  {
+    float8_t float8;
+    return float8;
+  }
+
+  float8_t float8_t::operator=(const uint8_t& _num)
+  {  
+    float8_t float8;
+    return float8;
+  }
+ 
+  float8_t float8_t::operator=(const int8_t& _num)
+  {
+    float8_t float8;
+    return float8;
+  }
+
+
+  float8_t::operator float()
+  {
+    std::cout << "float() " << uint32_t(m_num) << std::endl;
+    return float(m_num);
+  }
+
+  float8_t::operator uint64_t()
+  {
+    return m_num;
+  }
+
+  float8_t::operator int64_t()
+  {
+    return m_num;
+  }
+
+  float8_t::operator uint32_t()
+  {
+    return m_num;
+  }
+
+  float8_t::operator int32_t()
+  {
+    return m_num;
+  }
+
+  float8_t::operator uint16_t()
+  {
+    return m_num;
+  }
+
+  float8_t::operator int16_t()
+  {
+    return m_num;
+  }
+
+  float8_t::operator uint8_t()
+  {
+    return m_num;
+  }
+
+  float8_t::operator int8_t()
+  {
+    return m_num;
+  }
+
+  float8_t::operator char()
+  {
+    return m_num;
+  }
 }
+
--- elb/numeric/float8.hpp
+++ elb/numeric/float8.hpp
@@ -2,12 +2,36 @@
 #define ELB_FLOAT8_H_
 
 #include <cstdint>
+#include <cstdbool>
 
 namespace elb
 {
   struct float8_t
   {
     float8_t();
+
+    float8_t operator=(const float& _float);
+    float8_t operator=(const uint64_t& _num);
+    float8_t operator=(const int64_t& _num);
+    float8_t operator=(const uint32_t& _num);
+    float8_t operator=(const int32_t& _num);
+    float8_t operator=(const uint16_t& _num);
+    float8_t operator=(const int16_t& _num);
+    float8_t operator=(const uint8_t& _num);
+    float8_t operator=(const int8_t& _num);
+    float8_t operator=(const char& _num);
+
+    operator float();
+    operator uint64_t();
+    operator int64_t();
+    operator uint32_t();
+    operator int32_t();
+    operator uint16_t();
+    operator int16_t();
+    operator uint8_t();
+    operator int8_t();
+    operator char();
+
     int8_t m_num;
   };
 }
