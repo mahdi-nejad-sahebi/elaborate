1a0d7ae8c2706173e5628c7e95009ef3fbe501a0
Mahdi Nejad Sahebi <m.nejadsahebi@live.co.uk>
2024-01-01 06:29:16 +0330

    Operator test case were added.

--- tests/main.cpp
+++ tests/main.cpp
@@ -50,50 +50,33 @@ TEST(float8, read_write)
   }
 }
 
+TEST(float8, oeprators)
+{
+  elb::float8_t float8_1;
+  elb::float8_t float8_2;
+
+  for (uint32_t test_index = 0; test_index < 1000000; test_index++) {
+    float float32_1 = random_norm(elb::float8::FLT8_MIN, elb::float8::FLT8_MAX);
+    float float32_2 = random_norm(elb::float8::FLT8_MIN, elb::float8::FLT8_MAX);
+
+    float8_1 = float32_1;
+    float8_2 = float32_2;
+
+    float32_1 = float(float8_1);
+    float32_2 = float(float8_2);
+
+    EXPECT_EQ(float32_1 == float32_2, float8_1 == float8_2);
+    EXPECT_EQ(float32_1 != float32_2, float8_1 != float8_2);
+    EXPECT_EQ(float32_1 <  float32_2, float8_1 <  float8_2);
+    EXPECT_EQ(float32_1 <= float32_2, float8_1 <= float8_2);
+    EXPECT_EQ(float32_1 >  float32_2, float8_1 >  float8_2);
+    EXPECT_EQ(float32_1 >= float32_2, float8_1 >= float8_2);
+  }
+}
 
 int main()
 {
   return RUN_ALL_TESTS();
-  elb::float8_t f8;
-//  float f = 0;
-//  float fSrc = 0;
-  
-//  fSrc = -5.78F;
-//  f8 = fSrc;
-//  f = f8;
-//  std::cout << "Main: " << fSrc << ", " << uint32_t(f8.m_num) << ", " << f << std::endl;
-
-//  fSrc = 0.08F;
-//  f8 = fSrc;
-//  f = f8;
-//  std::cout << "Main: " << fSrc << ", " << uint32_t(f8.m_num) << ", " << f << std::endl;
-
-f8 = 76.54F;
-elb::float8_t g8;
-g8 = 76.54F; 
-bool t1 = (f8 == g8);std::cout << "bool: " << t1 << ", " << float(f8) << std::endl;
-f8 = 0.0F;
-bool t2 = (f8 == g8);std::cout << "bool: " << t2 << ", " << float(f8) << std::endl;
-  
-  bool b = false;
-  b = (45.0F < f8);std::cout << b << ", " << float(f8) << std::endl;
-  b = (f8 < -0.08F);std::cout << b << ", " << float(f8) << std::endl;
-  b = (f8 < 0.08F);std::cout << b << ", " << float(f8) << std::endl;
-
-
-  if (elb::float8::FLT8_MIN == f8)
-  {
-    std::cout << "min\n";
-  } 
-  else if (elb::float8::FLT8_MAX == f8)
-  {
-    std::cout << "max\n";
-  }
- // else if (elb::float8::FLT8_NAN == f8)
-  //  x1++;
-  
-
-  std::cout << "Test Done" << std::endl;
   return 0;
 }
 
