529dbee6af922b38d22df1b07f8b6d2bda92d114
Mahdi Nejad Sahebi <m.nejadsahebi@live.co.uk>
2024-01-07 08:54:46 +0330

    Test case of operator checking for float8 by float8 is written.

--- tests/main.cpp
+++ tests/main.cpp
@@ -45,41 +45,41 @@ static float random_norm(const float min, const float max)
 //  }
 //}
 
-TEST(float8, read_write)
-{
-  for (uint32_t test_index = 0; test_index < ITERATIONS_COUNT; test_index++) {
-    const float src_float32 = random_norm(elb::FLT8_MIN, elb::FLT8_MAX);
-    const elb::float8_t compressed_number = src_float32;
-    const float decompressed_number = float(compressed_number);
-
-    const float error = fabsf(src_float32 - decompressed_number);
-    if (compressed_number.is_norm())
-      EXPECT_LE(error, elb::FLT8_RESOLUTION_NORM);
-    else
-      EXPECT_LE(error, elb::FLT8_RESOLUTION);
-  }
-}
-
-//TEST(float8, oeprators_float8_by_float8)
+//TEST(float8, read_write)
 //{
 //  for (uint32_t test_index = 0; test_index < ITERATIONS_COUNT; test_index++) {
-//    float float32_1 = random_norm(elb::FLT8_MIN, elb::FLT8_MAX);
-//    float float32_2 = random_norm(elb::FLT8_MIN, elb::FLT8_MAX);
+//    const float src_float32 = random_norm(elb::FLT8_MIN, elb::FLT8_MAX);
+//    const elb::float8_t compressed_number = src_float32;
+//    const float decompressed_number = float(compressed_number);
 
-//    const elb::float8_t float8_1 = float32_1;
-//    const elb::float8_t float8_2 = float32_2;
-
-//    float32_1 = float(float8_1);
-//    float32_2 = float(float8_2);
-
-//    EXPECT_EQ(float32_1 == float32_2, float8_1 == float8_2);
-//    EXPECT_EQ(float32_1 != float32_2, float8_1 != float8_2);
-//    EXPECT_EQ(float32_1 <  float32_2, float8_1 <  float8_2);
-//    EXPECT_EQ(float32_1 <= float32_2, float8_1 <= float8_2);
-//    EXPECT_EQ(float32_1 >  float32_2, float8_1 >  float8_2);
-//    EXPECT_EQ(float32_1 >= float32_2, float8_1 >= float8_2);
+//    const float error = fabsf(src_float32 - decompressed_number);
+//    if (compressed_number.is_norm())
+//      EXPECT_LE(error, elb::FLT8_RESOLUTION_NORM);
+//    else
+//      EXPECT_LE(error, elb::FLT8_RESOLUTION);
 //  }
 //}
+
+TEST(float8, oeprators_float8_by_float8)
+{
+  for (uint32_t test_index = 0; test_index < ITERATIONS_COUNT; test_index++) {
+    const float src_float32_1 = random_norm(elb::FLT8_MIN, elb::FLT8_MAX);
+    const float src_float32_2 = random_norm(elb::FLT8_MIN, elb::FLT8_MAX);
+
+    const elb::float8_t float8_1 = src_float32_1;
+    const elb::float8_t float8_2 = src_float32_2;
+
+    const float float32_1 = float(float8_1);
+    const float float32_2 = float(float8_2);
+
+    EXPECT_EQ(float32_1 == float32_2, float8_1 == float8_2);
+    EXPECT_EQ(float32_1 != float32_2, float8_1 != float8_2);
+    EXPECT_EQ(float32_1 <  float32_2, float8_1 <  float8_2);
+    EXPECT_EQ(float32_1 <= float32_2, float8_1 <= float8_2);
+    EXPECT_EQ(float32_1 >  float32_2, float8_1 >  float8_2);
+    EXPECT_EQ(float32_1 >= float32_2, float8_1 >= float8_2);
+  }
+}
 
 
 //TEST(float8, oeprators_float8_by_float32)
